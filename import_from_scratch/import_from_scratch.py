6aef90378f3f666ae3a06cea6d89ed81e4785166
Daniel Ferreira <30530085+danielrferreira@users.noreply.github.com>
2023-08-12 09:42:52 -0300

    Update import_from_scratch.py

--- import_from_scratch/import_from_scratch.py
+++ import_from_scratch/import_from_scratch.py
@@ -1,25 +1,16 @@
 def import_file(input_file,header,delimiter=','):
-  #This function transform a txt file to a dictionary, given a header, a file and a delimiter(optional)  
-  with open(input_file,'r') as clash:
-        #gets the first line to check how many commas
+    '''This function transform a txt file to a dictionary, given a header, a file and a delimiter(optional)'''  
+    with open(input_file,'r') as clash:
+        #gets the first line to check how many delimiters
         temp=clash.readline() 
-        #creates a list with the same number of columns as squential elements
-        n_col=1
-        for c in temp: 
-            if c==',':
-                n_col+=1
+        n_col=temp.count(delimiter)+1
+        #Comeback to the beginning of the file
         clash.seek(0)
-        i=1
-        l=[0]
-        while i<n_col:
-            l.append(i)
-            i+=1
         result={1:1}
-        for g in l:
+        for g in range(n_col):
             result[header[g]]=[]
-        n_rows=len(clash.readlines())
-        
-        #Comeback the pointer to the begining of the file 
+        n_row=len(clash.readlines())
+        #Comeback to the beginning of the file
         clash.seek(0)
         #Line loop
         count_row=1
@@ -33,7 +24,7 @@ def import_file(input_file,header,delimiter=','):
                     start=pos+1 
                     count_var+=1
                     if count_var==n_col-1:
-                        if count_row==n_rows:
+                        if count_row==n_row:
                             result[header[count_var]].append(row[start:len(row)])
                         else: 
                             result[header[count_var]].append(row[start:len(row)-1])      
@@ -41,7 +32,7 @@ def import_file(input_file,header,delimiter=','):
             count_row+=1
     result.pop(1)
     return result
-
+  
 #%%
 #Function call example
 header = ('artist','album','country','region','year_artist','label','year_album')                    
@@ -50,9 +41,7 @@ db = import_file(input_file,header)
 
 #%%
 def retrieve(i):
-    print(db['album'][i],'by',db['artist'][i],'. Year of Release:', db['year_album'][i])           
-            
+    print(db['album'][i],'by',db['artist'][i],'. Year of Release:', db['year_album'][i])
+
 #%%
-
-retrieve(38)     
-
+retrieve(30)     
